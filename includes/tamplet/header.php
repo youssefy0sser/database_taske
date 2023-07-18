1b64341ac4c2266e7825c70d3ea765be286842ab
youssefy0sser <youssefabdelmwla@gmail.com>
2023-07-19 01:11:37 +0300

    Create header.php

---
+++ includes/tamplet/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./layout/css/<?php echo $css_file ;?>">
+    <title><?php echo $page_file ; ?></title>
+</head>
+<body>
